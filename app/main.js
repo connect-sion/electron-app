ef3a33ee782cc12d2cabed812a015caf62d3b59a
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-06-22 22:02:55 -0500

    Moves server to main process

--- app/main.js
+++ app/main.js
@@ -1,4 +1,42 @@
 const { app, BrowserWindow } = require('electron');
+const express = require('express');
+const server = express();
+var http = require('http').Server(server);
+var io = require('socket.io')(http);
+
+// signaling
+let broadcaster;
+io.on('connection', function (socket) {
+  socket.on('broadcaster', function () {
+    broadcaster = socket.id;
+    console.log('broadcaster', broadcaster);
+  });
+
+  socket.on('viewer', function () {
+    console.log('viewer', broadcaster, socket.id);
+    socket.to(broadcaster).emit('viewer', socket.id);
+  });
+
+  socket.on('candidate', function (id, event) {
+    console.log('candidate', id, socket.id);
+    socket.to(id).emit('candidate', socket.id, event);
+  });
+
+  socket.on('offer', function (id, event) {
+    console.log('offer', id, broadcaster);
+    socket.to(id).emit('offer', broadcaster, event.sdp);
+  });
+
+  socket.on('answer', function (event) {
+    console.log('answer', broadcaster, socket.id);
+    socket.to(broadcaster).emit('answer', socket.id, event.sdp);
+  });
+});
+
+// listener
+http.listen(8080, function () {
+  console.log('listening on 8080');
+});
 
 let mainWindow = null;
 
--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,6 @@
 const io = require('socket.io-client');
 
-const socket = io.connect('http://localhost:3000');
+const socket = io.connect('http://localhost:8080');
 
 const audio = document.querySelector('audio');
 let rtcPeerConnections = {};
